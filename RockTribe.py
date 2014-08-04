b6d2a06d53d4f99b11a88388a15f7174f4fd4c0f
Patrick Gorman <patrickgorman@patricks-mbp.netgear.com>
2014-08-04 11:36:08 -0400

    init

---
+++ RockTribe.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+import Models
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/pictures')
+def pictures():
+    images = Models.Picture.query.all()
+    return render_template("pictures.html", pictures=images)
+
+
+@app.route('/videos')
+def videos():
+    videoclips = Models.Video.query.all()
+    for video in videoclips:
+        if "http://" not in video.url:
+            video.url = "http://"+video.url
+        if "<br>" in video.description:
+            video.description = video.description.replace("<br>", "")
+    return render_template("videos.html", videos=videoclips)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
