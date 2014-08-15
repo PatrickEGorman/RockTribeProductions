bc359d48a06c098191f6c61a978d72322279a0ea
Patrick Gorman <gormanpe@vcu.edu>
2014-08-14 20:03:03 -0400

    Changed database port

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 
 if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = "postgresql://rocktribe:rocktribe@localhost:5433/rocktribe"
+    SQLALCHEMY_DATABASE_URI = "postgresql://rocktribe:rocktribe@localhost:5432/rocktribe"
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 DEBUG = True
