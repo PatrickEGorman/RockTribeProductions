fe4e45ffffee67cdc90060c93e513962f7ce3728
Patrick Gorman <gormanpe@vcu.edu>
2014-10-06 09:21:32 -0400

    Updated secret key

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app import app, views
 from app import config
+from app.config import secret_key
 from app.users import views
 from app.users.admins import views
 from app.users.comments import views
 
 if __name__ == '__main__':
+    app.secret_key = secret_key
     app.run(debug=True)
