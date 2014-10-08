17e0d48944bdec31b8a1a7795076c7a087e856b2
Patrick Gorman <gormanpe@vcu.edu>
2014-10-08 01:34:39 -0400

    changes

--- db_migrate.py
+++ db_migrate.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+import os;
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
